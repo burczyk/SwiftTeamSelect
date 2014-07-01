c30f8d7c26e0340038d951df41715354a773d6ec
Kamil Burczyk <kamil.burczyk@sigmapoint.pl>
2014-07-01 14:26:55 +0200

    Complete implementation of 1.0 version

--- SwiftTeamSelect/GameScene.swift
+++ SwiftTeamSelect/GameScene.swift
@@ -9,37 +9,261 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+    
+    enum Zone {
+        case Left, Center, Right
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+    var players = SKSpriteNode[]()
+    
+    var leftPlayer: SKSpriteNode?
+    var centerPlayer: SKSpriteNode?
+    var rightPlayer: SKSpriteNode?
+    
+    var leftGuide : CGFloat {
+        return round(view.bounds.width / 6.0)
+    }
+    
+    var rightGuide : CGFloat {
+        return view.bounds.width - leftGuide
+    }
+    
+    var gap : CGFloat {
+        return (size.width / 2 - leftGuide) / 2
+    }
+    
+    
+    // Initialization
+    
+    init(size: CGSize) {
+        super.init(size:size)
+        createPlayers()
+        centerPlayer = players[players.count/2]
+        setLeftAndRightPlayers()
+    }
+    
+    override func didMoveToView(view: SKView) {
+        placePlayersOnPositions()
+        calculateZIndexesForPlayers()
+    }
+    
+    func createPlayers() {
+        let flatUIColors = UIColor.flatUIColors()
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        for i in 0..9 {
+            let player = SKSpriteNode(color: flatUIColors[i], size: CGSizeMake(100, 200))
+            players.append(player)
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    func placePlayersOnPositions() {
+        for i in 0..players.count/2 {
+            players[i].position = CGPointMake(leftGuide, size.height/2)
+        }
+        
+        players[players.count/2].position = CGPointMake(size.width/2, size.height/2)
+
+        for i in players.count/2 + 1..players.count {
+            players[i].position = CGPointMake(rightGuide, size.height/2)
+        }
+        
+        for player in players {
+            player.setScale(calculateScaleForX(player.position.x))
+            self.addChild(player)
+        }
+        
     }
+    
+    
+    // Helper functions
+    
+    func calculateScaleForX(x:CGFloat) -> CGFloat {
+        let minScale = 0.5
+        
+        if x <= leftGuide || x >= rightGuide {
+            return minScale
+        }
+        
+        if x < size.width/2 {
+            let a = 1.0 / (size.width - 2 * leftGuide)
+            let b = 0.5 - a * leftGuide
+            
+            return (a * x + b)
+        }
+        
+        let a = 1.0 / (frame.size.width - 2 * rightGuide)
+        let b = 0.5 - a * rightGuide
+        
+        return (a * x + b)
+    }
+    
+    func calculateZIndexesForPlayers() {
+        var playerCenterIndex : Int = players.count / 2
+        
+        for i in 0..players.count {
+            if centerPlayer == players[i] {
+                playerCenterIndex = i
+            }
+        }
+        
+        for i in 0...playerCenterIndex {
+            players[i].zPosition = CGFloat(i)
+        }
+        
+        for i in playerCenterIndex+1..players.count {
+            players[i].zPosition = centerPlayer!.zPosition * 2 - CGFloat(i)
+        }
+
+    }
+    
+    func movePlayerToX(player: SKSpriteNode, x: CGFloat, duration: CGFloat) {
+        let moveAction = SKAction.moveToX(x, duration: duration)
+        let scaleAction = SKAction.scaleTo(calculateScaleForX(x), duration: duration)
+
+        player.runAction(SKAction.group([moveAction, scaleAction]))
+    }
+    
+    func movePlayerByX(player: SKSpriteNode, x: CGFloat) {
+        let duration = 0.01
+        
+        if CGRectGetMidX(player.frame) <= rightGuide && CGRectGetMidX(player.frame) >= leftGuide {
+            player.runAction(SKAction.moveByX(x, y: 0, duration: duration), completion: {
+                player.setScale(self.calculateScaleForX(CGRectGetMidX(player.frame)))
+            })
+            
+            if CGRectGetMidX(player.frame) < leftGuide {
+                player.position = CGPointMake(leftGuide, player.position.y)
+            } else if CGRectGetMidX(player.frame) > rightGuide {
+                player.position = CGPointMake(rightGuide, player.position.y)
+            }
+        }
+    }
+    
+    func zoneOfCenterPlayer() -> Zone {
+        let gap = size.width / 2 - leftGuide
+        
+        switch CGRectGetMidX(centerPlayer!.frame) {
+            
+        case let x where x < leftGuide + gap/2:
+            return .Left
+            
+        case let x where x > rightGuide - gap/2:
+            return .Right
+            
+        default: return .Center
+            
+        }
+    }
+    
+    func setLeftAndRightPlayers() {
+        var playerCenterIndex : Int = players.count / 2
+        
+        for i in 0..players.count {
+            if centerPlayer == players[i] {
+                playerCenterIndex = i
+            }
+        }
+        
+        if playerCenterIndex > 0 && playerCenterIndex < players.count {
+            leftPlayer = players[playerCenterIndex-1]
+        } else {
+            leftPlayer = nil
+        }
+        
+        if playerCenterIndex > -1 && playerCenterIndex < players.count-1 {
+            rightPlayer = players[playerCenterIndex+1]
+        } else {
+            rightPlayer = nil
+        }
+    }
+    
+    
+    
+    // Touch interactions
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        let touch = touches.anyObject() as UITouch
+        let node = self.nodeAtPoint(touch.locationInNode(self))
+        
+        if node == centerPlayer {
+            let fadeOut = SKAction.fadeAlphaTo(0.5, duration: 0.15)
+            let fadeIn = SKAction.fadeAlphaTo(1, duration: 0.15)
+            
+            centerPlayer!.runAction(fadeOut, completion: { self.centerPlayer!.runAction(fadeIn) })
+        }
+    }
+    
+    override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!) {
+        let duration = 0.01
+        let touch = touches.anyObject() as UITouch
+        let newPosition = touch.locationInNode(self)
+        let oldPosition = touch.previousLocationInNode(self)
+        let xTranslation = newPosition.x - oldPosition.x
+        
+        if CGRectGetMidX(centerPlayer!.frame) > size.width/2 {
+            if leftPlayer {
+                let actualTranslation = CGRectGetMidX(leftPlayer!.frame) + xTranslation > leftGuide ? xTranslation : leftGuide - CGRectGetMidX(leftPlayer!.frame)
+                movePlayerByX(leftPlayer!, x: actualTranslation)
+            }
+        } else {
+            if rightPlayer {
+                let actualTranslation = CGRectGetMidX(rightPlayer!.frame) + xTranslation < rightGuide ? xTranslation : rightGuide - CGRectGetMidX(rightPlayer!.frame)
+                movePlayerByX(rightPlayer!, x: actualTranslation)
+            }
+        }
+        
+        movePlayerByX(centerPlayer!, x: xTranslation)
+    }
+    
+    override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
+        let touch = touches.anyObject() as UITouch
+        let duration = 0.25
+        
+        switch zoneOfCenterPlayer() {
+            
+        case .Left:
+            if rightPlayer {
+                movePlayerToX(centerPlayer!, x: leftGuide, duration: duration)
+                if leftPlayer {
+                    movePlayerToX(leftPlayer!, x: leftGuide, duration: duration)
+                }
+                if rightPlayer {
+                    movePlayerToX(rightPlayer!, x: size.width/2, duration: duration)
+                }
+                
+                centerPlayer = rightPlayer
+                setLeftAndRightPlayers()
+            } else {
+                movePlayerToX(centerPlayer!, x: size.width/2, duration: duration)
+            }
+            
+        case .Right:
+            if leftPlayer {
+                movePlayerToX(centerPlayer!, x: rightGuide, duration: duration)
+                if rightPlayer {
+                    movePlayerToX(rightPlayer!, x: rightGuide, duration: duration)
+                }
+                if leftPlayer {
+                    movePlayerToX(leftPlayer!, x: size.width/2, duration: duration)
+                }
+                
+                centerPlayer = leftPlayer
+                setLeftAndRightPlayers()
+            } else {
+                movePlayerToX(centerPlayer!, x: size.width/2, duration: duration)
+            }
+            
+        case .Center:
+            movePlayerToX(centerPlayer!, x: size.width/2, duration: duration)
+            if leftPlayer {
+                movePlayerToX(leftPlayer!, x: leftGuide, duration: duration)
+            }
+            if rightPlayer {
+                movePlayerToX(rightPlayer!, x: rightGuide, duration: duration)
+            }
+        }
+        
+        calculateZIndexesForPlayers()
+    }
+    
 }
---
+++ SwiftTeamSelect/UIColor+Shorthands.swift
@@ -0,0 +1,112 @@
+//
+//  CGColor+Shorthands.swift
+//  SwiftTeamSelect
+//
+//  Created by Kamil Burczyk on 30.06.2014.
+//  Copyright (c) 2014 Sigmapoint. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    //convenience initializers
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat){
+        self.init(red:r/255.0, green:g/255.0, blue:b/255.0, alpha:a)
+    }
+    
+    convenience init(_ r: CGFloat, _ g: CGFloat, _ b: CGFloat){
+        self.init(red:r/255.0, green:g/255.0, blue:b/255.0, alpha:1)
+    }
+    
+    //array of all flatUI colors
+    
+    class func flatUIColors() -> UIColor[] {
+        return [UIColor.turquoise(), UIColor.emerald(), UIColor.peterRiver(), UIColor.amethyst(), UIColor.wetAsphalt(), UIColor.greenSea(), UIColor.nephritis(), UIColor.belizeHole(), UIColor.wisteria(), UIColor.midnightBlue(), UIColor.sunFlower(), UIColor.carrot(), UIColor.alizarin(), UIColor.clouds(), UIColor.concrete(), UIColor.orange(), UIColor.pumpkin(), UIColor.pomegranate(), UIColor.silver(), UIColor.asbestos()]
+    }
+    
+    //convenience methods to return flatUI colors
+    
+    class func turquoise() -> UIColor {
+        return UIColor(26, 188, 156)
+    }
+    
+    class func emerald() -> UIColor {
+        return UIColor(46, 204, 113)
+    }
+
+    class func peterRiver() -> UIColor {
+        return UIColor(52, 152, 219)
+    }
+    
+    class func amethyst() -> UIColor {
+        return UIColor(155, 89, 182)
+    }
+    
+    class func wetAsphalt() -> UIColor {
+        return UIColor(52, 73, 94)
+    }
+    
+    class func greenSea() -> UIColor {
+        return UIColor(22, 160, 133)
+    }
+    
+    class func nephritis() -> UIColor {
+        return UIColor(39, 174, 96)
+    }
+    
+    class func belizeHole() -> UIColor {
+        return UIColor(41, 128, 185)
+    }
+    
+    class func wisteria() -> UIColor {
+        return UIColor(142, 68, 173)
+    }
+    
+    class func midnightBlue() -> UIColor {
+        return UIColor(44, 62, 80)
+    }
+    
+    class func sunFlower() -> UIColor {
+        return UIColor(241, 196, 15)
+    }
+    
+    class func carrot() -> UIColor {
+        return UIColor(230, 126, 34)
+    }
+    
+    class func alizarin() -> UIColor {
+        return UIColor(231, 76, 60)
+    }
+    
+    class func clouds() -> UIColor {
+        return UIColor(236, 240, 241)
+    }
+    
+    class func concrete() -> UIColor {
+        return UIColor(149, 165, 166)
+    }
+ 
+    class func orange() -> UIColor {
+        return UIColor(243, 156, 18)
+    }
+    
+    class func pumpkin() -> UIColor {
+        return UIColor(211, 84, 0)
+    }
+    
+    class func pomegranate() -> UIColor {
+        return UIColor(192, 57, 43)
+    }
+    
+    class func silver() -> UIColor {
+        return UIColor(189, 195, 199)
+    }
+    
+    class func asbestos() -> UIColor {
+        return UIColor(127, 140, 141)
+    }
+    
+}
